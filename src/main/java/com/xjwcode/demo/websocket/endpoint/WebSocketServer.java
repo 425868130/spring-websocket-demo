10b10a543919bfd2bf879f5defc306443ba2f244
xujw <xujiawei@quklive.com>
2021-05-30 12:14:56 +0800

    update

--- src/main/java/com/xjwcode/demo/websocket/endpoint/WebSocketServer.java
+++ src/main/java/com/xjwcode/demo/websocket/endpoint/WebSocketServer.java
@@ -19,7 +19,7 @@ public class WebSocketServer {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private final static AtomicInteger onlineCount = new AtomicInteger(0);
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
-    private static final ConcurrentHashMap<String, WebSocketServer> webSocketMap = new ConcurrentHashMap<String, WebSocketServer>();
+    private static final ConcurrentHashMap<String, WebSocketServer> webSocketMap = new ConcurrentHashMap<>();
     /**
      * 与某个客户端的连接会话，需要通过它来给客户端发送数据
      */
